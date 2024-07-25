b8247326d9658cb9ce852e4f9f218c217085d9a4
Gabriel Fair <2096785+gabefair@users.noreply.github.com>
2024-07-25 08:17:37 -0400

    Add Right click to Archive feature

--- background.js
+++ background.js
@@ -1,9 +1,30 @@
 // background.js
 
-chrome.action.onClicked.addListener((tab) => {
-    const currentUrl = tab.url;
-    const encodedUrl = encodeURIComponent(currentUrl);
-    const proxyUrl = `https://archive.is/?run=1&url=${encodedUrl}`;
-    chrome.tabs.create({ url: proxyUrl });
-  });
+// Function to create a new tab with the proxied URL
+function openProxiedUrl(url) {
+  const encodedUrl = encodeURIComponent(url);
+  const proxyUrl = `https://archive.is/?run=1&url=${encodedUrl}`;
+  chrome.tabs.create({ url: proxyUrl });
+}
   
+  // Listener for the extension icon click
+chrome.action.onClicked.addListener((tab) => {
+  const currentUrl = tab.url;
+  openProxiedUrl(currentUrl);
+});
+
+// Create context menu for hyperlinks
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "proxy-link",
+    title: "Open link with Proxy",
+    contexts: ["link"]
+  });
+});
+
+// Listener for context menu click
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "proxy-link") {
+    openProxiedUrl(info.linkUrl);
+  }
+});
